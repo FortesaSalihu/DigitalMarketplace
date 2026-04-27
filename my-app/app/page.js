ce7cd439636c2f15830a863bb24de516f58be558
FortesaSalihu <fs70728@ubt-uni.net>
2026-04-28 00:33:57 +0200

    started home setup

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,23 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Checkbox from "@mui/material/Checkbox";
+"use client";
 
-const label = { slotProps: { input: { "aria-label": "Checkbox demo" } } };
 
+import Header from "@/components/header/Header"
 export default function Home() {
   return (
     <div>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-      <Checkbox {...label} defaultChecked />
-      <Checkbox {...label} />
-      <Checkbox {...label} disabled />
-      <Checkbox {...label} disabled checked />
+      <Header />
     </div>
   );
 }
