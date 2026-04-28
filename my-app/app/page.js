f2992273e9a07aec84c71b5cc726a6b156effc2c
FortesaSalihu <fs70728@ubt-uni.net>
2026-04-29 01:13:22 +0200

    finished home components

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,11 +1,14 @@
 "use client";
-
-
 import Header from "@/components/header/Header"
+import Category from "@/components/categories/CategoriesBar";
+import HeroSection from "@/components/home/hero/HeroSection"
+
 export default function Home() {
   return (
     <div>
       <Header />
+      <Category />
+      <HeroSection />
     </div>
   );
 }
